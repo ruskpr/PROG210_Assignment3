ab22a5e88e18594c9318afce468a8201657c77c4
Ronald Major <Ronald.Major@mymhc.ca>
2022-10-19 15:42:09 -0600

    Adding methods to User.

--- HackerNews/HackerNewsLibrary/User.cs
+++ HackerNews/HackerNewsLibrary/User.cs
@@ -25,5 +25,41 @@ namespace HackerNewsLibrary
             DateCreated = DateTime.Now;
         }
         #endregion
+
+        #region Methods
+
+        public bool CreateSubmission()
+        {
+            //user adds title, description, links to textboxes.
+            //information is submitted to Threads table in DB.
+            return true;
+        }
+
+        public bool AddComment()
+        {
+            //user adds title, and description to textboxes.
+            //information is submitted to Comments table in DB
+            return true;
+        }
+
+        public void upVote()
+        {
+            //user clicks upvote button.
+            //username is validated. 
+            //global upVote integer increments.
+        }
+
+        public void FavoriteSubmission()
+        {
+            //user clicks 'favorite' link / button on thread.
+            //threadID and info is written into Favorites table in DB.
+        }
+
+        public void Submitquestion()
+        {
+            //CreateSubmission with unique metadata for Question?
+        }
+
+        #endregion
     }
 }
