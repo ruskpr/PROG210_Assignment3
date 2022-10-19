d41229473f44ba2381f0b0fc25204a48cdb92e2e
Rustu Koprulu <rustulp@gmail.com>
2022-10-19 15:41:47 -0600

    Added properties to Thread class

--- HackerNews/HackerNewsLibrary/Thread.cs
+++ HackerNews/HackerNewsLibrary/Thread.cs
@@ -8,5 +8,22 @@ namespace HackerNewsLibrary
 {
     public class Thread
     {
+        public List<Comment> Comments = new List<Comment>();
+
+        public User Username { get; set; }
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+        public DateTime DateCreated { get; set; }
+
+
+        public Thread(User user, string title, string link, string desc)
+        {
+            Username = user;
+            Title = title;
+            Link = link;
+            Description = desc;
+            DateCreated = DateTime.Now;
+        }
     }
 }
