c76c74b46640dd9431b61689547b485c3a74c7c2
Rustu Koprulu <rustulp@gmail.com>
2022-10-21 09:49:46 -0600

    Added override tostring method

--- HackerNews/HackerNewsLibrary/Thread.cs
+++ HackerNews/HackerNewsLibrary/Thread.cs
@@ -10,14 +10,15 @@ namespace HackerNewsLibrary
     {
         public List<Comment> Comments = new List<Comment>();
 
-
+        #region Properties
         public User Username { get; set; }
         public string Title { get; set; }
         public string Link { get; set; }
         public string Description { get; set; }
-        public DateTime DateCreated { get; set; }
+        public DateTime DateCreated { get; }
+        #endregion
 
-
+        #region Constructor
         public Thread(User user, string title, string link, string desc)
         {
             Username = user;
@@ -26,5 +27,10 @@ namespace HackerNewsLibrary
             Description = desc;
             DateCreated = DateTime.Now;
         }
+        #endregion
+
+        public override string ToString() =>
+            $"{Username}'s thread";
+        
     }
 }
