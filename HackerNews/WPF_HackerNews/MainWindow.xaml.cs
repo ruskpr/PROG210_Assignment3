b549510101542c4a937b35491fa33908f68e06f9
Rustu Koprulu <rustulp@gmail.com>
2022-10-26 13:37:40 -0600

    save

--- HackerNews/WPF_HackerNews/MainWindow.xaml.cs
+++ HackerNews/WPF_HackerNews/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HackerNewsLibrary;
+using Thread = HackerNewsLibrary.Thread;
 
 namespace WPF_HackerNews
 {
@@ -20,9 +22,62 @@ namespace WPF_HackerNews
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static List<User> users = new List<User>();
+        public static List<Thread> threads = new List<Thread>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //generate list of threads
+            threads.AddRange(AddThreads());
+
+            // get threads from list and attach them to thread boxes
+            foreach (Thread t in threads)
+                ThreadBox.Boxes.Add(new ThreadBox(t));
+
+            //add to container
+            foreach (ThreadBox t in ThreadBox.Boxes)
+                stpContainer.Children.Add(t);
+                //wrpContainer.Children.Add(t);
+        }
+
+        // add users to list
+        private List<User> AddUsers()
+        {
+            List<User> users = new List<User>();
+
+            users.Add(new User("russ", "password", "russ@email.com"));
+            users.Add(new User("ron", "password", "ron@email.com"));
+
+            return users;
+        }
+
+        private List<Thread> AddThreads()
+        {
+            //get users from AddUsers method and attach them to thread
+            users.AddRange(AddUsers());
+            List<Thread> threads = new List<Thread>();
+
+            threads.Add(new Thread(users[1], "Inflation is going up.", "https://news.ycombinator.com/", "lorem ipsum."));
+            threads.Add(new Thread(users[0], "Global warming is a serious issue.", "https://www.youtube.com/", "lorem ipsum test."));
+
+            return threads;
+        }
+
+        private void lblnew_Copy_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //sort by new
+        }
+
+        private void lblpast_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+          //sort by old
+        }
+
+        private void lblthreads_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //search page
         }
     }
 }
---
+++ HackerNews/WPF_HackerNews/ThreadBox.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HackerNewsLibrary;
+using Thread = HackerNewsLibrary.Thread;
+
+
+namespace WPF_HackerNews
+{
+    /// <summary>
+    /// Interaction logic for ThreadBox.xaml
+    /// </summary>
+    public partial class ThreadBox : UserControl
+    {
+        #region Fields
+        //public static Bitmap? upVoteImg;
+        public static List<ThreadBox> Boxes = new List<ThreadBox>();
+        private int _index;
+        #endregion
+        #region Properties
+        public Thread TheThread { get; set; }
+        #endregion
+        #region Constructor 
+        public ThreadBox(Thread thread)
+        {
+            InitializeComponent();
+            //initialize button click event
+            //this.btnUpvote.MouseDown
+
+
+            VerticalContentAlignment = VerticalAlignment.Stretch;
+            //init upvote image
+            //upVoteImg = File.Exists("upvote.png") ? new Bitmap("upvote.png") : null;
+            //btnUpvote.Image = upVoteImg;
+
+            TheThread = thread;
+            _index = Boxes.Count + 1; // get index of thread based on current count of static list
+
+            UpdateThreadBox();
+        }
+        #endregion
+        #region Click events
+        private void lbTitle_MouseDown(object sender, MouseButtonEventArgs e) =>
+            MessageBox.Show("[Go to external link]");
+
+        private void lbDetails_MouseDown_1(object sender, MouseButtonEventArgs e) =>
+            MessageBox.Show("[Go to comments]");
+
+        private void BtnUpvote_MouseDown(object? sender, EventArgs e)
+        {
+            TheThread.UpVote();
+            UpdateThreadBox();
+        }
+        #endregion
+        #region Methods
+        public void UpdateThreadBox()
+        {
+            lbIndex.Content = $"{_index}.";
+            lbTitle.Content = TheThread.Title;
+            lbDetails.Content = $"{TheThread.UpVotes} points by {TheThread.Username} on " +
+                $"{TheThread.DateCreated} | {TheThread.Comments.Count} comments";
+        }
+
+        public override string ToString() => $"{TheThread.Username}'s Thread Box";
+
+        #endregion
+
+        
+    }
+}
